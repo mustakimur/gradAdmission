d2934064755c0024b875b032b9f3a9458b4e313e
Zhi Wang <zwang@cs.fsu.edu>
2018-06-11 10:58:01 -0400

    reformat documents

--- html/resources/local.js
+++ html/resources/local.js
@@ -12,7 +12,7 @@ function facts_app() {
     fields["interests"] = { tag: "Interests", can_change: true, convert: null, long: true };
     fields["ug_university"] = { tag: "UG University", can_change: true, convert: null, long: true };
     fields["ug_major"] = { tag: "UG Major", can_change: true, convert: null, long: true };
-    fields["ug_degree"] = { tag: "UG Degree", can_change: true, convert: null, long: false, select: ["B.S", "B.E", "B.A"]};
+    fields["ug_degree"] = { tag: "UG Degree", can_change: true, convert: null, long: false, select: ["B.S", "B.E", "B.A"] };
     fields["ug_gpa"] = { tag: "UG GPA", can_change: true, convert: parseFloat, long: false };
     fields["grad_university"] = { tag: "Grad University", can_change: true, convert: null, long: true };
     fields["grad_major"] = { tag: "Grad Major", can_change: true, convert: null, long: true };
@@ -20,9 +20,9 @@ function facts_app() {
     fields["grad_gpa"] = { tag: "Grad GPA", can_change: true, convert: parseFloat, long: false };
     fields["toefl_ielts"] = { tag: "TOEFL/IELTS", can_change: true, convert: parseInt, long: false };
     fields["gre"] = { tag: "GRE", can_change: true, convert: null, long: false };
-    fields["decision"] = { tag: "Decision", can_change: true, convert: null, long: true, select:["Pending", "Admit", "Admit w/ support", "Reject", "Accepted", "Denied", "Retracted"] };
+    fields["decision"] = { tag: "Decision", can_change: true, convert: null, long: true, select: ["Pending", "Admit", "Admit w/ support", "Reject", "Accepted", "Denied", "Retracted"] };
     fields["advisor"] = { tag: "Advisor", can_change: true, convert: null, long: false };
-    fields["assistantship"] = { tag: "Assistantship", can_change: true, convert: null, long: false, select:["None", "TA", "DRTA", "RA", "TA+RA", "SFS"] };
+    fields["assistantship"] = { tag: "Assistantship", can_change: true, convert: null, long: false, select: ["None", "TA", "DRTA", "RA", "TA+RA", "SFS"] };
     fields["fte"] = { tag: "FTE", can_change: true, convert: parseFloat, long: false };
     fields["yearly_amount"] = { tag: "Salary", can_change: true, convert: parseInt, long: false };
 
